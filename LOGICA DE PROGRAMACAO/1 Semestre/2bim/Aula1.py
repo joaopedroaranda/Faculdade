9177967371ee19da386d83a0eec86a88c7b117f2
joao0918 <joaopedroaranda19@gmail.com>
2024-05-01 13:01:13 -0400

    faculdade

---
+++ LOGICA DE PROGRAMACAO/1 Semestre/2bim/Aula1.py
@@ -0,0 +1,13 @@
+"""
+Esses são da aula da minha faculdade, 2 bimestre estamos estudando python
+"""
+
+a = int(input('Escreva um numero: '))
+b = int(input('Escreva outro numero: '))
+
+diferenca = a - b
+multiplicacao = a * b
+divisao = a / b
+soma = a + b
+
+print(f'diferenca: {diferenca}\nmultiplicacao: {multiplicacao}\ndivisao: {divisao}\nsoma: {soma}')
